27804529c15e6f72a8a004b0e8fa4bf81e26212e
beauspot <beauspot@outlook.com>
2023-02-24 20:16:18 +0100

    added and finished setting up 4 new middlewares.

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const morgan = require("morgan");
 const appRouter = require("./routes/contactRoutes");
 const _404Error = require("./controllers/_404");
 const ConnectDB = require("./db/connect");
+const helmet = require("helmet");
+const mongo_sanitize = require("express-mongo-sanitize");
+const cors = require("cors");
+const xss = require("xss-clean");
 require("dotenv").config();
 
 const app = express();
 
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(mongo_sanitize());
+app.use(cors());
+app.use(xss());
 
 app.use("/contacts", appRouter);
 
@@ -22,8 +30,7 @@ const PORT = process.env.PORT || 5050;
 
 const StartServer = async () => {
   try {
-    await ConnectDB(process.env.MONGO_URL);
-    console.info("Connected to the Database!");
+    await ConnectDB.connectDB(process.env.MONGO_URL);
     app.listen(PORT, () => {
       console.info(`Server listening on http://localhost:${PORT}`);
     });
